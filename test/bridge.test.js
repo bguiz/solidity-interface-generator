580e8379fc31f000eaca25660dc7c4fb5437b71b
Aleksandr Shenshin <shenshin@me.com>
2021-08-26 02:27:53 +0200

    Testing the compilation of the generated interface

--- test/bridge.test.js
+++ test/bridge.test.js
@@ -4,14 +4,16 @@ const assert = require('assert')
 const fs = require('fs-extra')
 const path = require('path')
 const exec = util.promisify(require('child_process').exec)
+const solc = require('solc')
+
+const getPath = (file) =>
+  path.resolve(__dirname, '..', 'contracts', file)
 
 describe('Bridge generator', () => {
   it('generates a bridge file', async () => {
     try {
       // delete previously created interface
-      await fs.remove(
-        path.resolve(__dirname, '..', 'contracts', 'Bridge.sol'),
-      )
+      await fs.remove(getPath('Bridge.sol'))
       // execute Terminal command
       const { stdout } = await exec('node index.js')
       // compare standart output with a certain string
@@ -19,6 +21,49 @@ describe('Bridge generator', () => {
         stdout,
         'The bridge was generated. Find it in the file ./contracts/Bridge.sol\n',
       )
+    } catch (error) {
+      assert(false, error.message)
+    }
+  })
+
+  it('compiles contracts using generated file', async () => {
+    try {
+      const contractFile = await fs.readFile(
+        getPath('Federation.sol'),
+        'utf8',
+      )
+      const bridgeFile = await fs.readFile(
+        getPath('Bridge.sol'),
+        'utf8',
+      )
+      const input = {
+        language: 'Solidity',
+        sources: {
+          'Federation.sol': {
+            content: contractFile,
+          },
+        },
+        settings: {
+          outputSelection: {
+            '*': {
+              '*': ['*'],
+            },
+          },
+        },
+      }
+      const findImports = (filePath) =>
+        filePath === 'Bridge.sol'
+          ? { contents: bridgeFile }
+          : { error: 'File not found' }
+      const compilation = JSON.parse(
+        solc.compile(JSON.stringify(input), { import: findImports }),
+      )
+      if (compilation.errors) {
+        throw new Error(compilation.errors[0].message)
+      }
+      const { 'Bridge.sol': bridge, 'Federation.sol': federation } =
+        compilation.contracts
+      assert(bridge && federation)
     } catch (error) {
       assert(false, error.message)
     }
